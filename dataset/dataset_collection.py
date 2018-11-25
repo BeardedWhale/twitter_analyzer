ee075c510e380b7fe997a071d716cb2bd3ab1a66
FrogTravel <e.levchenko@innopolis.ru>
2018-11-25 21:17:44 +0900

    Comments

--- dataset/dataset_collection.py
+++ dataset/dataset_collection.py
@@ -8,6 +8,7 @@ from watson_developer_cloud.natural_language_understanding_v1 import Features, C
 import pytz
 import twitter
 import datetime
+import json
 
 from dataset.constants import RETWEETED_STATUS_KEY, USER_KEY, SCREEN_NAME_KEY, USER_MENTIONS_KEY, \
     IN_REPLY_TO_SCREEN_NAME_KEY, RETWEETS_KEY, MENTIONS_KEY, COMMENTS_KEY
@@ -187,7 +188,6 @@ class TwitterApi():
             raise ValueError('Datetime should not be naive and should be in UTC (pytz.UTC or datetime.timezone.utc)')
         return date.strftime('%Y-%m-%dT%H:%M:%S%z')
 
-
     def a_is_follower_of_b(self, api, screen_name_a: str, screen_name_b: str):
         """
         Is screen_name_1 subscriber of screen_name_2?
@@ -247,12 +247,11 @@ class TwitterApi():
                     if friend_2.screen_name == friend_1.screen_name:
                         common_list.append(friend_1.screen_name)
 
-            return len(common_list)/friend_1
+            return len(common_list) / len(friends_1)
         except Exception as e:
             print(f'Exception occured: {e}')
 
         return -1
-
 
     def get_favorites_count(self, api, screen_name, screen_name2: str, since: datetime,
                             until: datetime = datetime.datetime.now(datetime.timezone.utc)):
@@ -294,7 +293,6 @@ class TwitterApi():
             print(f'Exception occured: {e}')
             return -1
 
-
     def parse_time_interval(self, since, until: datetime):
         if not since:
             since = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=30)
@@ -305,7 +303,6 @@ class TwitterApi():
             until = datetime.datetime.now(datetime.timezone.utc)
 
         return since, until
-
 
     # S
     def get_hashtags_similarity(self, posts1, posts2):
@@ -333,7 +330,6 @@ class TwitterApi():
                     count += 1
         return count / len(tags1)
 
-
     def _get_natural_language_understanding(self, version):
         n = NaturalLanguageUnderstandingV1(
             version=version,
@@ -341,7 +337,6 @@ class TwitterApi():
             url='https://gateway-wdc.watsonplatform.net/natural-language-understanding/api'
         )
         return n
-
 
     def _get_categories(self, posts):
         """
@@ -388,10 +383,147 @@ class TwitterApi():
         return count / len(categories1)
 
 
+class DatasetCollection():
+    def save_posts_of_user(self, twitterSearch, screen_names, file):
+        all_users = []
+        for screen_name in screen_names:
+
+            posts = twitterSearch.find_posts_twitter(api=twitterSearch.get_api_instance(), screen_name=screen_name,
+                                                     pool_amount=-1,
+                                                     since=datetime.datetime.now(
+                                                         datetime.timezone.utc) - datetime.timedelta(31),
+                                                     until=datetime.datetime.now(
+                                                         datetime.timezone.utc))  # TODO WTF is this time
+
+            # retweets = twitterSearch.find_retweets_twitter(posts=posts, screen_name=screen_name)
+            print(screen_name + "\n")
+            # print(str(retweets))
+            all_posts_with_user = dict()
+            # all_posts_with_user['screen_name'] = screen_name
+            # all_posts_with_user['user'] =
+            user = []
+            user['screen_name'] = screen_name
+            user['list_of_comments'] = []# лист кого комментировал
+            user['list_of_retweet'] = []# лист кого ретвител
+            user['list_of_mention'] = []# лист кого упоминал
+            user['list_of_likes'] = []# лист кого он лайкал
+            user['follow_to'] = []# кого он фолловит
+            user['description'] = ""# описание
+            user['creation_at'] = ""# дата создания
+            user['hashtag_list'] = []# хештег лист
+            user['categories_list'] = []# лист категорий
+
+            new_posts = []
+            for post in posts:
+                print("-")
+                if 'favorite_count' in post and 'retweet_count' in post:
+                    new_post = dict()
+
+                    new_post['create_at'] = post['created_at']
+                    new_post['text'] = post['text']
+                    new_post['id'] = post['id']
+                    new_post['hashtags'] = post['hashtags']
+                    new_post['lang'] = post['lang']
+                    new_post['retweet_count'] = post['retweet_count']
+                    new_post['favorite_count'] = post['favorite_count']
+                    new_post[RETWEETED_STATUS_KEY] = post.get(RETWEETED_STATUS_KEY, {})
+                    new_post[IN_REPLY_TO_SCREEN_NAME_KEY] = post.get(IN_REPLY_TO_SCREEN_NAME_KEY, "")
+                    new_post[USER_MENTIONS_KEY] = post.get(USER_MENTIONS_KEY, [])
+
+                    new_posts.append(new_post)
+
+                    # p = post['text'].replace("\"", " ")
+                    # file.write("{")
+                    # file.writelines(
+                    #     f"\'created_at\':\'{post['created_at']}\', "
+                    #     f"\'favorite_count\':{post['favorite_count']}, "
+                    #     f"\'id\':{post['id']}, "
+                    #     f"\'hashtags\':{post['hashtags']}, "
+                    #     f"\'lang\':\'{post['lang']}\', "
+                    #     f"\'retweet_count\':{post['retweet_count']}, "
+                    #     f"\'text\':\"{p}\"")
+                    # file.write("}")
+                    # index += 1
+                    # if index < len(posts):
+                    #     file.write(",")
+
+            all_posts_with_user['posts'] = new_posts
+            all_users.append(all_posts_with_user)
+
+        json_post = json.dumps(all_users)
+        file.write(json_post)
+
+    def read_users(self, file):
+        result_users = []
+        for line in file:
+            result_users.append(line)
+        return result_users
+
+    def _get_all_posts(self, screen_name):
+        posts = []
+        with open("posts.json") as json_posts:
+            file = json.load(json_posts)
+            posts = self._find_posts_by_screen_name(screen_name=screen_name, accounts_with_posts=file)
+
+        return posts
+
+    def _find_posts_by_screen_name(self, screen_name, accounts_with_posts):
+        for account in accounts_with_posts:
+            if screen_name == account['screen_name']:
+                return account['posts']
+
+        return -1
+
+    def find_similarity(self, twitterSearch, screen_name_1, screen_name_2, file):
+        posts1 = self._get_all_posts(screen_name=screen_name_1)
+        posts2 = self._get_all_posts(screen_name=screen_name_2)
+        print("--- Got Posts ---")
+
+        is_followed_by = twitterSearch.a_is_follower_of_b(api=twitterSearch.get_api_instance(),
+                                                          screen_name_a=screen_name_1,
+                                                          screen_name_b=screen_name_2)
+        s_creation_day = twitterSearch.similarity_creation_date(api=twitterSearch.get_api_instance(),
+                                                                screen_name_1=screen_name_1,
+                                                                screen_name_2=screen_name_2)
+        s_common_subscriptions = twitterSearch.common_subscriptions_a_to_b(api=twitterSearch.get_api_instance(),
+                                                                           screen_name_a=screen_name_1,
+                                                                           screen_name_b=screen_name_2)
+        print("--- Got Simple ---")
+
+        s_hashtag_similarity = twitterSearch.get_hashtags_similarity(posts1=posts1, posts2=posts2)
+        print("Hashtag finished")
+        s_categories_similarity = twitterSearch.get_categories_similarity(posts1=posts1, posts2=posts2)
+        print("---------")
+        file.write(f"similarity {screen_name_1} {screen_name_2}: [")
+        file.writelines(
+            f"is_followed_by={is_followed_by} is_followed_by={s_creation_day} s_common_subscriptions={s_common_subscriptions} "
+            f"s_hashtag_similarity={s_hashtag_similarity} s_categories_similarity={s_categories_similarity}]")
+
+
 if __name__ == '__main__':
     twitterSearch = TwitterApi()
-    posts = twitterSearch.find_posts_twitter(api=twitterSearch.get_api_instance(), screen_name='', pool_amount=50,
-                                             since=None)
+
     # pprint(posts)
-    interactions = twitterSearch.find_user_interactions_in_posts(posts, '')
-    pprint(interactions)
+
+
+    dataset = DatasetCollection()
+    # result_file = open("posts.json", "w")
+    # with open("accounts.txt") as file:
+    #     posts = []
+    #
+    #     posts.append(dataset.save_posts_of_user(twitterSearch=twitterSearch, screen_names=dataset.read_users(file),
+    #                                             file=result_file))
+
+
+
+    s_file = open("similarity_file.json", "w")
+    with open("accounts.txt") as file1:
+        for line1 in file1:
+            with open("accounts.txt") as file2:
+                for line2 in file2:
+                    if line1 != line2:
+                        dataset.find_similarity(twitterSearch=twitterSearch, screen_name_1=line1, screen_name_2=line2,
+                                                file=s_file)
+
+                        # print(twitterSearch.common_subscriptions_a_to_b(api=twitterSearch.get_api_instance(), screen_name_a="NASA",
+                        #                                                 screen_name_b="SpaceX"))
